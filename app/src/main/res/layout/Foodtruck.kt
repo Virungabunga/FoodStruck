054454400758649020034dfbfa886719494c6be3
josefinhellgren <josefin.hellgren@gmail.com>
2022-10-13 14:54:36 +0200

    Added Foodtruck class

---
+++ app/src/main/res/layout/Foodtruck.kt
@@ -0,0 +1,10 @@
+package layout
+
+class Foodtruck(var companyName: String, var bigPicId: Int) {
+
+}
+
+
+/*var companyName: String,var availability : Boolean, var longPos : Double, var latPos : Double,
+                var bigPicId: Int, var smallPic: Int, var rating : Int, var distance : Int, var bioText : String,
+                var phoneNr: Int, var webpage : String*/
