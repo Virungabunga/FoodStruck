671e2f1dda988b8ca21c69d76e636f5430793f9b
josefinhellgren <josefin.hellgren@gmail.com>
2022-10-13 15:00:42 +0200

    la till Foodtruck klass

--- app/src/main/java/edu/ith/foodstruck/Foodtruck.kt
+++ app/src/main/java/edu/ith/foodstruck/Foodtruck.kt
@@ -1,10 +1,9 @@
-package layout
+package edu.ith.foodstruck
 
-class Foodtruck(var companyName: String, var bigPicId: Int) {
+class Foodtruck (var companyName: String, var bigPicId: Int) {}
 
-}
 
 
 /*var companyName: String,var availability : Boolean, var longPos : Double, var latPos : Double,
                 var bigPicId: Int, var smallPic: Int, var rating : Int, var distance : Int, var bioText : String,
-                var phoneNr: Int, var webpage : String*/
+                var phoneNr: Int, var webp
--- app/src/main/java/edu/ith/foodstruck/MainActivity.kt
+++ app/src/main/java/edu/ith/foodstruck/MainActivity.kt
@@ -11,6 +11,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_maps)
         val db = Firebase.firestore
 
-        
+
     }
 }
