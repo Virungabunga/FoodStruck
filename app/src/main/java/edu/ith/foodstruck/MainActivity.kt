d1379cb10f894017ba68516e57bbcc002130631c
oscarsparrman <oscar.sparrman@student.kyh.se>
2022-10-13 13:41:33 +0200

    La in firestore och maps funktion

--- app/src/main/java/edu/ith/foodstruck/MainActivity.kt
+++ app/src/main/java/edu/ith/foodstruck/MainActivity.kt
@@ -2,10 +2,13 @@ package edu.ith.foodstruck
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_maps)
+        val db = Firebase.firestore
     }
 }
