9e94144989040826da8c1177964cb76a84a1e802
josefinhellgren <josefin.hellgren@gmail.com>
2022-10-18 12:35:40 +0200

    Tagit bort överflödig aktivtet och bytt map till Main

--- app/src/main/java/edu/ith/foodstruck/MainActivity.kt
+++ app/src/main/java/edu/ith/foodstruck/MainActivity.kt
@@ -2,14 +2,55 @@ package edu.ith.foodstruck
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import edu.ith.foodstruck.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_maps)
         val db = Firebase.firestore
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+
+
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+
+        val pelles = LatLng(59.0, 18.0)
+        mMap.addMarker(
+            MarkerOptions().position(pelles)
+                .title("pelles foodtruck")
+                .icon(
+                    BitmapDescriptorFactory.fromResource(
+                        R.drawable.smallicon
+                    )
+                )
+        )
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(pelles))
 
 
     }
