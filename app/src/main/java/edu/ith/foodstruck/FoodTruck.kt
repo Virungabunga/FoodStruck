a0ca7b3a26c6073b957eacd6b868a9d421e2ab0c
oscarsparrman <oscar.sparrman@student.kyh.se>
2022-10-18 15:27:32 +0200

    Vi la till klasss idatabas och läste till kartaw

---
+++ app/src/main/java/edu/ith/foodstruck/FoodTruck.kt
@@ -0,0 +1,8 @@
+import com.google.firebase.firestore.DocumentId
+
+data class FoodTruck (var companyName: String? = null,
+                      var smallPicId: Int? = null,
+                      var long : Double? = null,
+                      var lat : Double?=null,
+                      @DocumentId var documentId: String? = null) {}
+
--- app/src/main/java/edu/ith/foodstruck/MainActivity.kt
+++ app/src/main/java/edu/ith/foodstruck/MainActivity.kt
@@ -1,5 +1,6 @@
 package edu.ith.foodstruck
 
+import FoodTruck
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -11,7 +12,11 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.FieldPath.documentId
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.ktx.Firebase
 import edu.ith.foodstruck.databinding.ActivityMainBinding
 
@@ -20,10 +25,11 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMainBinding
-
+    private lateinit var db:FirebaseFirestore
+    private lateinit var oscarFoodTruck:FoodTruck
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val db = Firebase.firestore
+         db = Firebase.firestore
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -32,6 +38,12 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
+        oscarFoodTruck = FoodTruck("Oscars Mackor",
+            R.drawable.smallicon,
+            59.3609701472053, 17.96979995865056,)
+
+        db.collection("FoodTruck")
+            .add(oscarFoodTruck)
 
 
     }
@@ -39,19 +51,47 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
+       val food1 = db.collection("FoodTruck")
+           .get()
+            .addOnSuccessListener { documentSnapshot ->
+                val foodTruckList = mutableListOf<FoodTruck>()
 
-        val pelles = LatLng(59.0, 18.0)
+                for (document in documentSnapshot.documents) {
+                    val truck = document.toObject<FoodTruck>()
+                    if (truck != null)
+                        foodTruckList.add(truck)
+
+                }
+
+               var long = foodTruckList[0].long
+                var  lat = foodTruckList[0].lat
+
+                if (long != null) {
+                    if (lat != null) {
+                        addMarker(lat,long)
+                    }
+                }
+
+           }
+
+    }
+
+
+
+    fun addMarker(lat:Double,long :Double) {
+
         mMap.addMarker(
-            MarkerOptions().position(pelles)
-                .title("pelles foodtruck")
+            MarkerOptions().position(LatLng(long,lat))
+                .title("")
                 .icon(
                     BitmapDescriptorFactory.fromResource(
                         R.drawable.smallicon
                     )
                 )
         )
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(pelles))
-
-
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(LatLng(long,lat)))
     }
+
 }
+
+
